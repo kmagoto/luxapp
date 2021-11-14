ecb342b9b9fbef740e640782b3cab48780bf4903
kmagoto <kennedymagoto@gmail.com>
2021-11-14 13:36:41 +0300

    update

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ name = "Lux Academy - By Kennedy Magoto"
 
 print(f"Hello everyone welcome to {name}")
 
-prinf("This is a simple Python app that has been built using the FastAPI web framework and Docker.\n The app is a basic application, as you can see.")
+print("This is a simple Python app that has been built using the FastAPI web framework and Docker.\n The app is a basic application, as you can see.")
