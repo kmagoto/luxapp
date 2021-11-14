60dc8214718d27e3480bf73a53f08e31ae88db5c
kmagoto <kennedymagoto@gmail.com>
2021-11-14 13:45:58 +0300

    code update

--- server.py
+++ server.py
@@ -6,35 +6,11 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return { "Lux app": "Welcome to Lux app"
-    		###def my_function(x, y, z):
-###    """
-###>>> my_function(2, 3, addition)
-###5
-###>>> my_function(0, 5, multiplication)
-###0
-###    """
-### return()
-  
+    return { from pystrich.datamatrix import DataMatrixEncoder
 
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
-z = str(input("Select the operator you want among division, multiplication, addition, subtraction, modulus: "))
-
-addition = x+y
-multiplication = x*y
-division = x/y
-subtraction = x-y
-
-if z == "addition":
-    print("x added to y is: {}".format(addition))
-elif z == "subtraction":
-    print("x subtracted y is: {}".format(subtraction))
-elif z == "division":
-    print("x divided by y is: {}".format(division))
-elif z == "multiplication":
-    print("x multiplied by y is: {}".format(multiplication))
-    		 } 
+encoder = DataMatrixEncoder('This is a DataMatrix.')
+encoder.save('./datamatrix_test.png')
+print(encoder.get_ascii()) } 
 
 @app.post("/login/")
 async def login(username: str = Form(...), password: str = Form(...)):
